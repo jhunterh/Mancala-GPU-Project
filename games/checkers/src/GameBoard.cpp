95b3c5853527e3c8b546c5b2aa69919858675262
Tristan Jay <capdincrando@gmail.com>
2023-03-17 14:25:02 -0500

    Add getBoardResult for Checkers

--- games/checkers/src/GameBoard.cpp
+++ games/checkers/src/GameBoard.cpp
@@ -2,6 +2,13 @@
 
 namespace Game {
 
+    
+// TODO: CAN_JUMP(oldPos, newPos) function
+// TODO: CAN_MOVE(oldPos, newPos) function
+// OR make jump list
+// TODO: IS_VALID(oldPos, newPos) function
+static const boardpos_t jumpOffsets[4] = {-9, -7, 7, 9};
+
 // Init the board state
 void GameBoard::initBoard()
 {
@@ -15,22 +22,73 @@ void GameBoard::initBoard()
 // Execute a move on the board for a given player
 CUDA_CALLABLE_MEMBER moveresult_t GameBoard::executeMove(move_t move, Player::playernum_t playerNum)
 {
-    // Implement code here
-    return 0;
+    // Currently unused
+    // TODO: Add invalid move checking
+    (void) playerNum;
+
+    // Handle old space
+    // TODO: Check if old space is occupied and correct player
+    bitboard_t oldMask = (1 << move.oldPos);
+    boardState.isOccupiedBoard &= ~oldMask; // Clear old board state
+
+    // Handle new space
+    // TODO: Check if new space is empty
+    bitboard_t newMask = (1 << move.newPos);
+    boardState.isOccupiedBoard |= newMask;
+    if(boardState.isBlackBoard & newMask)
+        boardState.isBlackBoard |= newMask;
+    else
+        boardState.isBlackBoard &= ~newMask;
+    if(boardState.isKingBoard & newMask)
+        boardState.isKingBoard |= newMask;
+    else
+        boardState.isKingBoard &= ~newMask;
+    
+    // Handle jump
+    // TODO: Check if jump space is occupied and other player
+    bool isJump = (move.jumpPos != -1);
+    boardState.isOccupiedBoard &= ~(isJump << move.jumpPos);  // Clear jump spot if jump
+
+    // Handle move again
+    // TODO: Add check to do another jump
+    return MoveResult::MOVE_SUCCESS;
 }
 
 // Return the possible move on the board for a given player
 CUDA_CALLABLE_MEMBER movecount_t GameBoard::getMoves(movelist_t& movesOut, Player::playernum_t playerNum)
 {
     // Implement code here
-    return 0;
+    movecount_t moveCount = 0;
+    movecount_t jumpCount = 0;
+    movelist_t moveList;
+    movelist_t jumpList;
+    for(boardpos_t pos = 0; pos < GAME_BOARD_SIZE; pos++)
+    {
+        bitboard_t mask = (1 << pos);
+        if(boardState.isOccupiedBoard & mask)
+        {
+            
+        }
+    }
+    return moveCount;
 }
 
 // Return the board result
-CUDA_CALLABLE_MEMBER boardresult_t GameBoard::getBoardResult()
+CUDA_CALLABLE_MEMBER boardresult_t GameBoard::getBoardResult(Player::playernum_t currentPlayerNum)
 {
-    // Implement code here
-    return 0;
+    // Get other player num
+    playernum_t otherPlayerNum = !currentPlayerNum;
+
+    // Get move counts
+    movelist_t moves;
+    bool canCurrentPlayerMove = getMoves(moves, currentPlayerNum);
+    bool canOtherPlayerMove = getMoves(moves, otherPlayerNum);
+
+    // Calculate board result
+    boardresult_t result = GAME_ACTIVE;
+    result += (currentPlayerNum + 1)*(canCurrentPlayerMove) + 
+                (otherPlayerNum + 1)*(canOtherPlayerMove);
+    return result;
 }
 
 // Return the state of the board in string format
