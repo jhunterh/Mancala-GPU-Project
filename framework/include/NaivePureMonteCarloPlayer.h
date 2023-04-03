a1ae28a9e51e089c3c6a181e07d08c85b2d1431c
jhunterh <44873307+jhunterh@users.noreply.github.com>
2023-04-02 20:37:10 -0500

    12 pure mcts on gpu (#21)
    * initial Pure MCTS no optimization, performance metrics, or unit testing yet
    
    * initial performance metrics for PMCTS
    
    * added naive PMCTS player for unit testing
    
    * added unit test for pure MCTS
    
    * fix segfault with Pure MCTS
    
    ---------
    
    Co-authored-by: Hunter <hunter@hunterpc.attlocal.net>

---
+++ framework/include/NaivePureMonteCarloPlayer.h
@@ -0,0 +1,52 @@
+#ifndef _NAIVEPUREMONTECARLOPLAYER_H
+#define _NAIVEPUREMONTECARLOPLAYER_H
+
+#include <vector>
+
+#include "RandomPlayer.h"
+#include "MonteCarloTypes.h"
+#include "MonteCarloUtility.h"
+
+namespace Player {
+
+// Definition of Naive Pure Monte Carlo Player
+// This player selects a move based on the Pure Monte Carlo Search Algorithm
+class NaivePureMonteCarloPlayer : public Player {
+public:
+    NaivePureMonteCarloPlayer();
+    ~NaivePureMonteCarloPlayer() = default;
+
+    player_t getPlayerType() override { return 5; }
+	std::string getDescription() override { return "Naive Pure Monte Carlo Player"; }
+	Game::move_t selectMove(Game::GameBoard& board, playernum_t playerNum);
+    std::vector<MonteCarlo::SimulationPerformanceReport> getSimulationReports() {return m_simulationReports;}
+    std::string getPerformanceDataString() override;
+
+    // unit testing interface
+    void setDeterministic(bool isPreDetermined, int value) 
+    {
+        m_randomPlayer->setDeterministic(isPreDetermined, value);
+    }
+
+    void setNumSimulations(int num)
+    {
+        m_numSimulations = num;
+    }
+
+    void setRootNode(std::shared_ptr<MonteCarlo::TreeNode> node)
+    {
+        m_rootNode = node;
+    }
+
+    void runSimulation(unsigned int& simulationResults, unsigned int& simulationNumMoves, int moveNum);
+
+private:
+    int m_numSimulations = 1000;
+    std::shared_ptr<RandomPlayer> m_randomPlayer;
+    std::shared_ptr<MonteCarlo::TreeNode> m_rootNode = nullptr;
+    std::vector<MonteCarlo::SimulationPerformanceReport> m_simulationReports;
+};
+
+}
+
+#endif
